29833f9dd379861ee96e6ab24db9f0813e18329d
Junin13 <86210507+Junin13@users.noreply.github.com>
2024-04-18 12:57:11 -0300

    Delete calculadora.js

--- calculadora.js
+++
@@ -1,8 +0,0 @@
-// calculadora.js
-
-module.exports = {
-    somar: (a, b) => parseFloat(a) + parseFloat(b),
-    subtrair: (a, b) => parseFloat(a) - parseFloat(b),
-    multiplicar: (a, b) => parseFloat(a) * parseFloat(b),
-    dividir: (a, b) => parseFloat(a) / parseFloat(b)
-};
