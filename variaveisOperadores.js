8c9184624cbf0168aa51e86c345e89a6c11c3b8f
Junin13 <86210507+Junin13@users.noreply.github.com>
2024-04-18 12:31:56 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,22 @@
+
+let numA = 10;
+let numB = 5;
+
+
+let resultado;
+
+
+resultado = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB}: ${resultado}`);
+
+
+resultado = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB}: ${resultado}`);
+
+
+resultado = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB}: ${resultado}`);
+
+
+resultado = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB}: ${resultado}`);
